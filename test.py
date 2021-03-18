77c253d43b70fe94a56c0e78e98c5163ffa6a160
riadcsa <65103146+riadcsa@users.noreply.github.com>
2021-03-17 22:33:21 -0400

    Add files via upload

---
+++ test.py
@@ -0,0 +1,17 @@
+#list, tuple, dictionary, forzenset, set
+our_list = ["a", "b", "c", 1.5]
+print(our_list)
+
+# tuple is same as list but only immutable
+
+our_tuple = ("a", "b", "c")
+print(our_tuple)
+
+our_dictionary = {"name":"DarkChild", "height":"6'0"}
+print(our_dictionary)
+
+our_set = {"one", "two", "three"}
+print(our_set)
+
+
+
